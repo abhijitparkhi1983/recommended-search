016fea23cf1a197541b6d5edb23b768a90ed785c
hyejineee <dev.hyejin04@gmail.com>
2023-01-12 06:40:16 +0900

    settings : reset css 설정

---
+++ src/reset.styles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset} 
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    input, textarea { 
+      -moz-user-select: auto;
+      -webkit-user-select: auto;
+      -ms-user-select: auto;
+      user-select: auto;
+    }
+
+    button {
+      border: none;
+      background: none;
+      padding: 0;
+      cursor: pointer;
+    }
+`;
+
+export default GlobalStyles;
